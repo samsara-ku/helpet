095adeb672b4fec6680a25d0e7055ef0986d2fac
yunhoe <yunhoe@deepnatural.ai>
2020-12-19 02:32:15 +0900

    [feature/1] Home 기본 스트럭쳐 생성 #1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,52 +1,68 @@
-import './Home.css';
-import React from 'react';
+import './Home.scss';
+import React, { useState } from 'react';
 import GradeIcon from '@material-ui/icons/Grade';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import SearchForm from './SearchForm';
 
 function Home() {
+  const mockData = [
+    {
+      id: '1234',
+      title: 'Hi',
+    },
+    {
+      id: '5436',
+      title: 'Hello',
+    },
+    {
+      id: '2315',
+      title: 'Good',
+    },
+    {
+      id: '0966',
+      title: 'We',
+    },
+    {
+      id: '7546',
+      title: 'Are',
+    },
+    {
+      id: '2109',
+      title: 'Best',
+    },
+    {
+      id: '1034',
+      title: 'Doing',
+    },
+  ];
+
+  const [iter, setIter] = useState([0, 1, 2]);
+
+  const handleClick = e => {
+    setIter(iter.map(elem => (elem + e + mockData.length) % mockData.length));
+  };
+
   return (
     <div className="home">
       <div className="home__logo">
         <GradeIcon />
       </div>
-
       <div className="home__search-area">
         <SearchForm />
       </div>
-
       <div className="home__content">
+        {iter}
         <h3 className="home__content__title">인기 콘텐츠</h3>
-        <div className="home__contnet__grid">
-          {[1, 2, 3, 4, 5, 6].map((e) => (
-            <div className="home__content__card border">
-              <img src={`https://via.placeholder.com/200x200?text=${e}`} alt="" />
-              <span>안녕{e}</span>
+        <div className="home__content__grid">
+          <ChevronLeftIcon onClick={e => handleClick(-1)} />
+          {iter.map(e => (
+            <div className="home__content__card">
+              <img src="https://via.placeholder.com/200x200?text=$" alt="" />
+              <div>{mockData[e].title}</div>
             </div>
           ))}
-        </div>
-      </div>
-
-      <div className="home__content">
-        <h3 className="home__content__title">인기 콘텐츠</h3>
-        <div className="home__contnet__grid">
-          {[1, 2, 3, 4, 5, 6].map((e) => (
-            <div className="home__content__card border">
-              <img src={`https://via.placeholder.com/200x200?text=${e}`} alt="" />
-              <span>안녕{e}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="home__content">
-        <h3 className="home__content__title">인기 콘텐츠</h3>
-        <div className="home__contnet__grid">
-          {[1, 2, 3, 4, 5, 6].map((e) => (
-            <div className="home__content__card border">
-              <img src={`https://via.placeholder.com/200x200?text=${e}`} alt="" />
-              <span>안녕{e}</span>
-            </div>
-          ))}
+          <ChevronRightIcon onClick={e => handleClick(+1)} />
         </div>
       </div>
     </div>
