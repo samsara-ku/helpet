432605f642e4f09f8ca8aa19bb459323e31aee68
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-12-24 14:10:22 +0900

    [feature/11] Articles 파일 생성 #11

---
+++ src/components/Information/Articles.jsx
@@ -0,0 +1,14 @@
+import './Articles.scss';
+import React from 'react';
+
+function Articles() {
+  return (
+    <div className="articles">
+      <div>메타</div>
+      <div>목록</div>
+      <div>페이지네이션</div>
+    </div>
+  );
+}
+
+export default Articles;
