146c4390cde4d33f75d43473e983ee31e3753a9f
chinsung 200713 <chin_sung@naver.com>
2020-12-24 10:52:01 +0900

    [feature/12] Rename Article.scss to ArticleDetail.scss #12
    리네임 및 레퍼런스 수정
    
    jsx와 다르게 수동으로 바꿔줘야한다.

--- src/components/Information/ArticleDetail.jsx
+++ src/components/Information/ArticleDetail.jsx
@@ -1,25 +1,20 @@
-import './Article.scss';
+import './ArticleDetail.scss';
 import React from 'react';
 import HomeIcon from '@material-ui/icons/Home';
 import { Link } from 'react-router-dom';
 
 function ArticleDetail() {
   return (
-    <div className="article border">
-      <div className="article__breadcrumb">
-        <HomeIcon />
+    <div className="article-detail border">
+      <div className="article-detail__breadcrumb">
+        <HomeIcon className />
         <span>홈/강아지 연구소/강아지 행동/강아지 보호자를 화나게 하는 말 5가지</span>
       </div>
-
-      <div className="article__title">
+      <div className="article-detail__title">
         <h1>타이틀</h1>
       </div>
-      <div className="article__meta">
-        <span className="article__meta__date">4일전</span>
-        <span className="article__meta__read-time">2분 읽을거리</span>
-      </div>
 
-      <div className="article__content">
+      <div className="article-detail__content">
         {Array(10)
           .fill('')
           .map(() => (
@@ -30,14 +25,10 @@ function ArticleDetail() {
           ))}
       </div>
 
-      <div className="article__ad">
-        <img src="https://via.placeholder.com/600x160?text=ad" alt="" />
-      </div>
-
-      <div className="article__tags">
+      <div className="article-detail__tags">
         {['강아지 산책', '강아지 주인', '강아지 행동'].map(e => (
           <Link to="/">
-            <span className="article__tag boder">{e}</span>
+            <span className="article-detail__tag boder">{e}</span>
           </Link>
         ))}
       </div>
