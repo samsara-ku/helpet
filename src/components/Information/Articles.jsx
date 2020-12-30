5ce344f9362d3fc21c4568cceec81f49ac6fc77a
chinsung 200713 <chin_sung@naver.com>
2020-12-30 21:32:32 +0900

    [feature/21] Articles 서버에서 실제 데이터 쿼리하기 #21

--- src/components/Information/Articles.jsx
+++ src/components/Information/Articles.jsx
@@ -1,9 +1,38 @@
 import './Articles.scss';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Article from './Article';
 import Pagination from './Pagination';
 
 function Articles() {
+  const [articlePreviewList, setArticlePreviewList] = useState([]);
+
+  useEffect(async () => {
+    const endpoint = 'http://localhost:5000/graphql';
+    const query = `
+    {
+      articles {
+          aidx,
+          title,
+          summary,
+          thumbnail,
+          insert_date
+        },
+    }
+    `;
+
+    // fetch 로 graphql 요청 보내기
+    // 결과로 받는 객체 data.{쿼리객체명} 을 적으면 결과를 꺼낼 수 있음.
+    const result = (
+      await fetch(endpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query }),
+      }).then(res => res.json())
+    ).data.articles;
+
+    setArticlePreviewList(result);
+  }, []);
+
   return (
     <div className="articles">
       <div className="articles__meta">
@@ -12,11 +41,20 @@ function Articles() {
         </span>
       </div>
       <div className="articles__content">
-        {Array(5)
-          .fill('')
-          .map((item, idx) => (
-            <Article key={idx} />
-          ))}
+        {articlePreviewList.map((item, idx) => {
+          const { aidx, title, summary, thumbnail, insert_date: insertDate } = item;
+
+          return (
+            <Article
+              key={idx}
+              aidx={aidx}
+              title={title}
+              summary={summary}
+              thumbnail={thumbnail}
+              insertDate={insertDate}
+            />
+          );
+        })}
       </div>
 
       <Pagination current="6" total="10" baseUrl="" />
