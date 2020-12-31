1acdcc61a28f76a1053c0aae79f327a8c3f790f6
baehoons <qogns314@naver.com>
2020-12-31 13:31:10 +0900

    testing branch

--- src/components/Global/Footer/Footer.jsx
+++ src/components/Global/Footer/Footer.jsx
@@ -8,7 +8,10 @@ function Footer() {
       <div className="footer__row">helpet© Copyright 2020, All Rights Reserved</div>
       <div className="footer__row">
         <div className="footer__col">
-          <Link to="/">이용약관</Link>
+          <Link to="/">이용약관_testing</Link>
+        </div>
+        <div>
+          Test 입니다
         </div>
         <div className="footer__col">
           <Link to="/">개인정보처리방침</Link>
