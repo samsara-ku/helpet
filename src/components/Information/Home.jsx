94f2880a3e33442af1b9b9aa0fc3c2927f21f607
yunhoe <yunhoe@deepnatural.ai>
2021-01-19 16:43:37 +0900

    [feature/33] Graphql 내용 Home.jsx에 라우팅 #33

--- src/components/Information/Home.jsx
+++ src/components/Information/Home.jsx
@@ -6,63 +6,40 @@ import Search from './Search';
 import Menu from './Menu';
 
 function Home() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  const [articlePreviewList, setArticlePreviewList] = useState([]);
 
-  const mockData = [
-    {
-      content:
-        'A new rule from the Department of Transportation now officially restricts the definition of service animals traveling with airline passengers with a disability to be dogs. More specifically, the rule states the service animal must be “a dog, regardless of breed or type, that is individually trained to do work or perform tasks for the benefit of a qualified individual with a disability, including a physical, sensory, psychiatric, intellectual, or other mental disability.”',
-      title: 'Service Animals on Planes Now Officially Limited to Dogs',
-      time: '12:11:50',
-      imgPath:
-        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*',
-    },
-    {
-      content:
-        'Health insurance is the one thing in life you need but hope you never have to use: It’s the best safeguard against unforeseen illness, injury, and all the other potential pitfalls of being alive. For a growing number of pet insurance customers, the sentiment holds true for our beloved companion animals. But the pet insurance space, which seems to add more companies and options to choose from by the day, can be confusing and overwhelming to navigate.',
-      title: 'The Best Pet Insurance: A Pet Parent’s Guide',
-      time: '12:11:50',
-      imgPath:
-        'https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg',
-    },
-    {
-      content:
-        'December brings many welcome changes, including the “Age of Aquarius” that’s coming our way later in the month. This cosmic event will usher in wide-sweeping changes allowing opportunities for healing, transformation, and higher-minded, humanitarian pursuits. We’ll start to connect more—even it’s virtually—with the aim of building a better world together.',
-      title: 'December Pet Horoscopes: What the Stars Have in Store for Dogs and Cats',
-      time: '12:11:50',
-      imgPath: 'https://i.ytimg.com/vi/7xh1DAKIdng/maxresdefault.jpg',
-    },
-    {
-      content: '0966',
-      title: 'The Top 10 Smartest Dog Breeds',
-      time: '12:11:50',
-      imgPath:
-        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-puppy-lying-down-on-grass-royalty-free-image-1587052215.jpg?crop=1.00xw:0.754xh;0,0.166xh&resize=980:*',
-    },
-    {
-      content: '7546',
-      title: 'The 18 Cutest Mixed-Breed Dogs',
-      time: '12:11:50',
-      imgPath:
-        'https://images.mypetlife.co.kr/content/uploads/2019/07/10150937/IMG_59781-e1562739086699-610x446.jpg',
-    },
-    {
-      content: '2109',
-      title: '31 Cute Pictures of All-Star Dogs',
-      time: '12:11:50',
-      imgPath:
-        'https://images.mypetlife.co.kr/content/uploads/2019/07/10153003/IMG_9464-610x813.jpg',
-    },
-    {
-      content: '1034',
-      title: 'The Breeds to Watch at the Westminster Dog Show',
-      time: '12:11:50',
-      imgPath:
-        'https://images.mypetlife.co.kr/content/uploads/2019/07/18175456/adorable-angry-animal-208984-e1563440212415-390x220.jpg',
-    },
-  ];
+  useEffect(async () => {
+    const endpoint = 'https://helpet-backend.herokuapp.com/graphql';
+    const query = `
+      {
+        articles {
+            aidx,
+            title,
+            summary,
+            thumbnail,
+            insert_date
+          },
+      }
+      `;
+
+    // fetch 로 graphql 요청 보내기
+    // 결과로 받는 객체 data.{쿼리객체명} 을 적으면 결과를 꺼낼 수 있음.
+    const result = (
+      await fetch(endpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query }),
+      }).then(res => res.json())
+    ).data.articles;
+
+    setArticlePreviewList(result);
+
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, []);
 
   return (
     <div className="home">
@@ -78,52 +55,58 @@ function Home() {
       <div className="home__content">
         <div className="home__content__title">인기 콘텐츠</div>
         <div className="home__content__box popular">
-          {mockData
+          {articlePreviewList
             .filter((e, i) => i < 3)
             .map(e => (
-              <div className="home__content__post">
-                <div className="home__content__post-col">
-                  <img src={e.imgPath} alt="" />
+              <Link to={`./article/${e.aidx}`}>
+                <div className="home__content__post">
+                  <div className="home__content__post-col">
+                    <img src={e.thumbnail} alt="" />
+                  </div>
+                  <div className="home__content__post-col">
+                    <div className="home__content__post-title">{e.title}</div>
+                    <div className="home__content__post-content">{e.summary}</div>
+                  </div>
                 </div>
-                <div className="home__content__post-col">
-                  <div className="home__content__post-title">{e.title}</div>
-                  <div className="home__content__post-content">{e.content}</div>
-                </div>
-              </div>
+              </Link>
             ))}
         </div>
       </div>
       <div className="home__content">
         <div className="home__content__title">집사 필수 정보</div>
         <div className="home__content__box essential">
-          {mockData
+          {articlePreviewList
             .filter((e, i) => i < 4)
             .map(e => (
-              <div className="home__content__post">
-                <div className="home__content__post-col">
-                  <img src={e.imgPath} alt="" />
+              <Link to={`./article/${e.aidx}`}>
+                <div className="home__content__post">
+                  <div className="home__content__post-col">
+                    <img src={e.thumbnail} alt="" />
+                  </div>
+                  <div className="home__content__post-col">
+                    <div className="home__content__post-title">{e.title}</div>
+                  </div>
                 </div>
-                <div className="home__content__post-col">
-                  <div className="home__content__post-title">{e.title}</div>
-                </div>
-              </div>
+              </Link>
             ))}
         </div>
       </div>
       <div className="home__content">
         <div className="home__content__title">이런건 어때요?</div>
         <div className="home__content__box etc">
-          {mockData
-            .filter((e, i) => i !== 2 && i !== 0)
+          {articlePreviewList
+            .filter((e, i) => i >= 5)
             .map(e => (
-              <div className="home__content__post">
-                <div className="home__content__post-col">
-                  <img src={e.imgPath} alt="" />
+              <Link to={`./article/${e.aidx}`}>
+                <div className="home__content__post">
+                  <div className="home__content__post-col">
+                    <img src={e.thumbnail} alt="" />
+                  </div>
+                  <div className="home__content__post-col">
+                    <div className="home__content__post-title">{e.title}</div>
+                  </div>
                 </div>
-                <div className="home__content__post-col">
-                  <div className="home__content__post-title">{e.title}</div>
-                </div>
-              </div>
+              </Link>
             ))}
         </div>
       </div>
