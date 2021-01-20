4b26ac7de0ff9d04e132229eec44f64290ca2dec
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2021-01-20 14:32:52 +0900

    [feature/29] 슬라이드 완성
    1. 하단 버튼 추가
    2. 1초 마다 자동슬라이드
    3. 슬라이드 영역에 호버여부에 따라 슬라이드 일시정지 및 재시작

--- src/components/Adoptation/Home.jsx
+++ src/components/Adoptation/Home.jsx
@@ -1,5 +1,5 @@
 import './Home.scss';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TweenLite, Power3 } from 'gsap';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -9,9 +9,10 @@ const MAX = 1;
 const MIN = 0;
 
 function Home() {
-  let testimonialList = useRef(null);
+  const testimonialList = useRef(null);
   const [slideState, setSlideState] = useState(1); // 0 1
   const [isAnimating, setIsAnimating] = useState(false);
+  const [isHover, setIsHover] = useState(false);
 
   const nextSlide = () => {
     if (isAnimating) {
@@ -20,11 +21,12 @@ function Home() {
 
     const prevState = slideState;
     const nextState = slideState + 1 > MAX ? MIN : slideState + 1;
+    setSlideState(nextState);
 
     setIsAnimating(true);
     // 이전 화면 왼쪽으로 보내기
     TweenLite.fromTo(
-      testimonialList.children[prevState],
+      testimonialList.current.children[prevState],
       {
         x: 0,
         opacity: 1,
@@ -40,7 +42,7 @@ function Home() {
 
     // 다음화면 불러오기
     TweenLite.fromTo(
-      testimonialList.children[nextState],
+      testimonialList.current.children[nextState],
       {
         x: `100%`,
         opacity: 0.5,
@@ -52,7 +54,6 @@ function Home() {
         duration: 1,
         zIndex: 10,
         onComplete: () => {
-          setSlideState(nextState);
           setIsAnimating(false);
         },
       }
@@ -65,11 +66,12 @@ function Home() {
     }
     const prevState = slideState;
     const nextState = slideState - 1 < MIN ? MAX : slideState - 1;
+    setSlideState(nextState);
 
     setIsAnimating(true);
     // 이전 화면 오른쪽으로 보내기
     TweenLite.fromTo(
-      testimonialList.children[prevState],
+      testimonialList.current.children[prevState],
       {
         x: 0,
         opacity: 1,
@@ -85,7 +87,7 @@ function Home() {
 
     // 다음화면 불러오기
     TweenLite.fromTo(
-      testimonialList.children[nextState],
+      testimonialList.current.children[nextState],
       {
         x: `-100%`,
         opacity: 0.5,
@@ -97,20 +99,50 @@ function Home() {
         duration: 1,
         zIndex: 10,
         onComplete: () => {
-          setSlideState(nextState);
           setIsAnimating(false);
         },
       }
     );
   };
 
+  const goSlide = e => {
+    console.log(e.target);
+    const el = e.target;
+
+    if (el.classList.contains('active')) {
+      console.log(`이미 현 화면입니다`);
+      return 0;
+    }
+
+    // 해당 화면으로 이동
+    nextSlide();
+  };
+
+  useEffect(() => {
+    const sto = setTimeout(() => {
+      console.log(`nextSlide();`);
+      if (!isHover) {
+        nextSlide();
+      }
+    }, 1000);
+
+    return () => {
+      console.log(`clearTimeout(sto);`);
+      clearTimeout(sto);
+    };
+  }, [testimonialList, nextSlide, isHover]);
+
   return (
     <div className="adopt__home">
       <div
-        className="home__route-container"
-        ref={el => {
-          testimonialList = el;
+        onMouseEnter={() => {
+          setIsHover(true);
         }}
+        onMouseLeave={() => {
+          setIsHover(false);
+        }}
+        className="home__route-container"
+        ref={testimonialList}
       >
         <div className={`home__route-container-item${slideState === 0 ? ' active' : ''}`}>
           <img src="http://www.saac.kr/images/kor09r-17-0380/main/main_visual01.jpg" alt="" />
@@ -135,6 +167,26 @@ function Home() {
           <button type="button" onClick={nextSlide}>
             <ChevronRightIcon />
           </button>
+        </div>
+
+        <div className="labels">
+          <div
+            role="button"
+            aria-hidden="true"
+            onClick={goSlide}
+            className={`label${slideState === 0 ? ' active' : ''}`}
+          >
+            입양 정보
+          </div>
+
+          <div
+            role="button"
+            aria-hidden="true"
+            onClick={goSlide}
+            className={`label${slideState === 1 ? ' active' : ''}`}
+          >
+            입양 후기
+          </div>
         </div>
       </div>
     </div>
