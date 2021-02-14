d9f7da42aec14ebdfa5c80c784a89ca0ac46d285
yunhoe <yunhoe@deepnatural.ai>
2021-02-14 23:40:54 +0900

    [feature/5] className 수정 #5

--- src/components/Store/Home/index.jsx
+++ src/components/Store/Home/index.jsx
@@ -5,32 +5,32 @@ function Home() {
   return (
     <div className="store__home">
       <div className="store__home__row">
-        <div className="divider__wrapper">
+        <div className="divider-wrapper">
           <span className="divider" />
         </div>
         <div className="title">반려동물 용품</div>
-        <div className="content__wrapper">2</div>
-        <div className="button__wrapper">
+        <div className="content-wrapper">2</div>
+        <div className="button-wrapper">
           <button type="button">더 보기 +</button>
         </div>
       </div>
       <div className="store__home__row">
-        <div className="divider__wrapper">
+        <div className="divider-wrapper">
           <span className="divider" />
         </div>
         <div className="title">굿즈</div>
-        <div className="content__wrapper">2</div>
-        <div className="button__wrapper">
+        <div className="content-wrapper">2</div>
+        <div className="button-wrapper">
           <button type="button">더 보기 +</button>
         </div>
       </div>
       <div className="store__home__row">
-        <div className="divider__wrapper">
+        <div className="divider-wrapper">
           <span className="divider" />
         </div>
         <div className="title">다양한 카테고리</div>
-        <div className="content__wrapper">2</div>
-        <div className="button__wrapper">
+        <div className="content-wrapper">2</div>
+        <div className="button-wrapper">
           <button type="button">더 보기 +</button>
         </div>
       </div>
