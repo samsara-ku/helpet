6758d5a209687d59c072a8d79eb27923fa614cf2
yunhoe <yunhoe@deepnatural.ai>
2021-02-15 02:52:27 +0900

    [feature/5] Item 컴포넌트 작성 완성 및 scss 추가 / Store 랜딩 페이지 작업 완료 #5

--- src/components/Store/Home/index.jsx
+++ src/components/Store/Home/index.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 import './index.scss';
+import Item from '../Item';
 
 function Home() {
+  const mockData = [
+    {
+      title: '고양이용 스크래쳐',
+      image:
+        'https://koozpetshop.com/web/product/medium/202012/ed2915d1f708af93e24a07ed03088d5e.jpg',
+      price: '45,000',
+      rating: 3,
+      reviewNum: '12',
+    },
+    {
+      title: '고양이용 사료',
+      image: 'https://koozpetshop.com/web/product/medium/2b122.jpg',
+      price: '24,000',
+      rating: 4,
+      reviewNum: '1',
+    },
+    {
+      title: '강아지용 스낵볼',
+      image: 'https://koozpetshop.com/web/product/medium/tobpet1222_sum_009.jpg',
+      price: '9,550',
+      rating: 1,
+      reviewNum: '3',
+    },
+    {
+      title: '강아지용 노즈워크 동글과녁',
+      image: 'https://koozpetshop.com/web/product/medium/tobpet_210114_sum_012.jpg',
+      price: '32,000',
+      rating: 2,
+      reviewNum: '0',
+    },
+  ];
+
   return (
     <div className="store__home">
       <div className="store__home__row">
@@ -9,7 +42,18 @@ function Home() {
           <span className="divider" />
         </div>
         <div className="title">반려동물 용품</div>
-        <div className="content-wrapper">2</div>
+        <div className="content-wrapper">
+          {mockData.map((e, i) => (
+            <Item
+              key={i}
+              title={e.title}
+              image={e.image}
+              price={e.price}
+              reviewNum={e.reviewNum}
+              rating={e.rating}
+            />
+          ))}
+        </div>
         <div className="button-wrapper">
           <button type="button">더 보기 +</button>
         </div>
@@ -19,7 +63,18 @@ function Home() {
           <span className="divider" />
         </div>
         <div className="title">굿즈</div>
-        <div className="content-wrapper">2</div>
+        <div className="content-wrapper">
+          {mockData.map((e, i) => (
+            <Item
+              key={i}
+              title={e.title}
+              image={e.image}
+              price={e.price}
+              reviewNum={e.reviewNum}
+              rating={e.rating}
+            />
+          ))}
+        </div>
         <div className="button-wrapper">
           <button type="button">더 보기 +</button>
         </div>
@@ -29,9 +84,23 @@ function Home() {
           <span className="divider" />
         </div>
         <div className="title">다양한 카테고리</div>
-        <div className="content-wrapper">2</div>
-        <div className="button-wrapper">
-          <button type="button">더 보기 +</button>
+        <div className="content-wrapper">
+          <div className="content">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/7/70/Dog_silhouette.svg"
+              alt="thumbnail"
+            />
+            <span>강아지 용품</span>
+            <button type="button">보러가기</button>
+          </div>
+          <div className="content">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png"
+              alt="thumbnail"
+            />
+            <span>고양이 용품</span>
+            <button type="button">보러가기</button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Store/Item/index.jsx
+++ src/components/Store/Item/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import './index.scss';
 
 function Item({ image, title, price, reviewNum, rating }) {
   return (
     <div className="store__item">
       <img className="item__thumbnail" src={image} alt="thumbnail" />
       <div className="item__title">{title}</div>
-      <div className="item__rating">{rating}</div>
-      <div className="item_review">(리뷰 수: {reviewNum})</div>
-      <div className="item__price">{price}</div>
-      <div className="button-wrapper">
-        <button type="button">1</button>
+      <div className="item__rating">
+        {new Array(5 - rating).fill(0).map((e, i) => (
+          <StarBorderIcon key={i} />
+        ))}
+        {new Array(rating).fill(0).map((e, i) => (
+          <StarIcon key={i} />
+        ))}
+      </div>
+      <div className="item__review">(리뷰 수: {reviewNum})</div>
+      <div className="item__price">₩{price}</div>
+      <div className="item__button">
+        <button type="button">장바구니</button>
       </div>
     </div>
   );
