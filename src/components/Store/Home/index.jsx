11213bfeeaa13fc50de289cf840f5b490e5ce220
yunhoe <yunhoe@deepnatural.ai>
2021-02-14 23:00:46 +0900

    [feature/5] 스토어 아이템을 제외한 내용 마크업 #5

--- src/components/Store/Home/index.jsx
+++ src/components/Store/Home/index.jsx
@@ -4,7 +4,36 @@ import './index.scss';
 function Home() {
   return (
     <div className="store__home">
-      <div>This area is for store</div>
+      <div className="store__home__row">
+        <div className="divider__wrapper">
+          <span className="divider" />
+        </div>
+        <div className="title">반려동물 용품</div>
+        <div className="content__wrapper">2</div>
+        <div className="button__wrapper">
+          <button type="button">더 보기 +</button>
+        </div>
+      </div>
+      <div className="store__home__row">
+        <div className="divider__wrapper">
+          <span className="divider" />
+        </div>
+        <div className="title">굿즈</div>
+        <div className="content__wrapper">2</div>
+        <div className="button__wrapper">
+          <button type="button">더 보기 +</button>
+        </div>
+      </div>
+      <div className="store__home__row">
+        <div className="divider__wrapper">
+          <span className="divider" />
+        </div>
+        <div className="title">다양한 카테고리</div>
+        <div className="content__wrapper">2</div>
+        <div className="button__wrapper">
+          <button type="button">더 보기 +</button>
+        </div>
+      </div>
     </div>
   );
 }
