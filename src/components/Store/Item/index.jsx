fb97ee8090d28f8aeb2d3e14f8b05c65f9d8b538
yunhoe <yunhoe@deepnatural.ai>
2021-02-16 03:08:28 +0900

    [feature/5] 장바구니 버튼이 상품 페이지로 연결되는 문제 해결 #5

--- src/components/Store/Item/index.jsx
+++ src/components/Store/Item/index.jsx
@@ -10,19 +10,19 @@ function Item({ image, title, price, rating }) {
       <Link to={`/product/${title}/`}>
         <img className="item__thumbnail" src={image} alt="thumbnail" />
         <div className="item__title">{title}</div>
-        <div className="item__rating">
-          {new Array(5 - rating).fill(0).map((e, i) => (
-            <StarBorderIcon key={i} />
-          ))}
-          {new Array(rating).fill(0).map((e, i) => (
-            <StarIcon key={i} />
-          ))}
-        </div>
-        <div className="item__price">₩{price}</div>
-        <div className="item__button">
-          <button type="button">장바구니</button>
-        </div>
       </Link>
+      <div className="item__rating">
+        {new Array(5 - rating).fill(0).map((e, i) => (
+          <StarBorderIcon key={i} />
+        ))}
+        {new Array(rating).fill(0).map((e, i) => (
+          <StarIcon key={i} />
+        ))}
+      </div>
+      <div className="item__price">₩{price}</div>
+      <div className="item__button">
+        <button type="button">장바구니</button>
+      </div>
     </div>
   );
 }
