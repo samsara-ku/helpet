2f80c4b730a8ebea1f3418005c831983472b5e22
chinsung 200713 <chin_sung@naver.com>
2021-02-18 16:55:39 +0900

    [feature/39] 로그인 기능 작성중 #39

--- src/components/Information/SignIn.jsx
+++ src/components/Information/SignIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.scss';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
@@ -6,6 +6,26 @@ import CancelIcon from '@material-ui/icons/Cancel';
 import Modal from '../Global/Modal/Modal';
 
 function SignIn({ isOpen, close }) {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onSigninHandler = async () => {
+    console.log(formState);
+    const result = await fetch(`http://localhost:5000/signin`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formState),
+    }).then(res => res.json());
+
+    console.log(result);
+  };
+
+  const onChangeHandler = (name, value) => {
+    setFormState({ ...formState, [name]: value });
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={() => close()}>
       <div className="sign-in">
@@ -20,16 +40,36 @@ function SignIn({ isOpen, close }) {
               <TwitterIcon />
             </div>
             <div className="sign-in__login-email">
-              <input type="text" placeholder="Email" />
+              <input
+                type="text"
+                placeholder="Email"
+                name="email"
+                value={formState.email}
+                onChange={e => {
+                  const { name, value } = e.target;
+                  onChangeHandler(name, value);
+                }}
+              />
             </div>
             <div className="sign-in__login-password">
-              <input type="text" placeholder="Password" />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formState.password}
+                onChange={e => {
+                  const { name, value } = e.target;
+                  onChangeHandler(name, value);
+                }}
+              />
             </div>
             <div className="sign-in__login-forgot">
               <u>비밀번호</u>를 잊으셨나요?
             </div>
             <div className="sign-in__login-button">
-              <button type="button">로그인</button>
+              <button type="button" onClick={onSigninHandler}>
+                로그인
+              </button>
             </div>
           </div>
         </div>
